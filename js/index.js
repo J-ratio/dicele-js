4d9e005b66b15df9848b0ccbe09cd735df14dabc
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2023-02-17 02:12:33 +0530

    Issue fixes 17-02-23
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let tutorial = false;
+
 function openSideBar() {
     document.getElementById("sidebar").style.display = "flex";
     document.getElementById("overlay").style.display = "block";
@@ -28,6 +30,48 @@ function openModal(modalId) {
 
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
+    if (tutorial && modalId === "help-modal") {
+        const source =
+            document.querySelectorAll(`[data-value]`)[
+                state.findIndex((value, index) => value != solution[index])
+            ];
+        const dest =
+            document.querySelectorAll(`[data-value]`)[
+                solution.findIndex(
+                    (value, index) =>
+                        value == source.getAttribute("data-value") &&
+                        state[index] != solution[index]
+                )
+            ];
+
+        document.querySelector(".tut-hand").style.left =
+            source.getBoundingClientRect().x +
+            source.getBoundingClientRect().width / 2 +
+            "px";
+        document.querySelector(".tut-hand").style.top =
+            source.getBoundingClientRect().y +
+            source.getBoundingClientRect().height / 2 +
+            "px";
+        document.querySelector(".tut-hand").style.display = "block";
+        document
+            .querySelector(".tut-hand")
+            .style.setProperty(
+                "--translateX",
+                `${
+                    dest.getBoundingClientRect().x -
+                    source.getBoundingClientRect().x
+                }px`
+            );
+        document
+            .querySelector(".tut-hand")
+            .style.setProperty(
+                "--translateY",
+                `${
+                    dest.getBoundingClientRect().y -
+                    source.getBoundingClientRect().y
+                }px`
+            );
+    }
 }
 
 const SpawnArr = [
@@ -440,6 +484,8 @@ function initBoard() {
 
 if (newUser) {
     initBoard();
+    openModal("help-modal");
+    tutorial = true;
 } else {
     document.querySelector(".moves-number").innerHTML = moves + 5 * adCount;
     document.querySelector(".game-number").innerHTML = `${level + 1}`;
@@ -622,6 +668,11 @@ var sortable = Sortable.create(board, {
         if (moves === 0 || moves === -5 * adCount) {
             showLoseScreen();
         }
+
+        if (tutorial) {
+            document.querySelector(".tut-hand").style.display = "none";
+            tutorial = false;
+        }
     },
 });
 
@@ -692,14 +743,6 @@ function showWinScreen() {
         updateLocalStorage();
     }
     document.querySelector("[data-field=score]").innerHTML = score;
-    document.querySelector(
-        "[data-rank-type=level] > .ranking-grid > [data-field=score-self]"
-    ).innerHTML = highScore;
-    let overallScore = 0;
-    levels.forEach((level) => (overallScore += level.score));
-    document.querySelector(
-        "[data-rank-type=global] > .ranking-grid > [data-field=score-self]"
-    ).innerHTML = overallScore;
     axios
         .post(`${backendURL}/DiceleCompleted/dicelecompleted`, {
             userId: userID,
@@ -716,7 +759,7 @@ function showWinScreen() {
         .then((response) => response.data)
         .then((data) => {
             document
-                .querySelectorAll(".ranking-entry[data-rank-type=level]")
+                .querySelectorAll(".ranking-entry[data-rank-type=daily]")
                 .forEach((ele, index) => {
                     ele.innerHTML = `
                         <div class="ranking-grid">
@@ -728,7 +771,7 @@ function showWinScreen() {
                         </div>`;
                 });
             document
-                .querySelectorAll(".ranking-entry[data-rank-type=global]")
+                .querySelectorAll(".ranking-entry[data-rank-type=all-time]")
                 .forEach((ele, index) => {
                     ele.innerHTML = `
                         <div class="ranking-grid">
@@ -741,16 +784,25 @@ function showWinScreen() {
                 });
 
             document.querySelector(
-                ".ranking-self[data-rank-type=level] > .ranking-grid > [data-field=rank-self]"
+                ".ranking-self[data-rank-type=daily] > .ranking-grid > [data-field=rank-self]"
             ).innerHTML = data.daily[3].rank;
             document.querySelector(
-                ".ranking-self[data-rank-type=global] > .ranking-grid > [data-field=rank-self]"
+                ".ranking-self[data-rank-type=all-time] > .ranking-grid > [data-field=rank-self]"
+            ).innerHTML = data.overall[3].rank;
+            document.querySelector(
+                "[data-rank-type=daily] > .ranking-grid > [data-field=score-self]"
+            ).innerHTML = data.daily[3].rank;
+            document.querySelector(
+                "[data-rank-type=all-time] > .ranking-grid > [data-field=score-self]"
             ).innerHTML = data.overall[3].rank;
         })
         .catch((err) => console.error(err));
     openModal("win-modal");
     level++;
     initBoard();
+    party.confetti(document.querySelector(".win-banner"), {
+        count: 100,
+    });
 }
 
 function showLoseScreen() {
@@ -761,55 +813,110 @@ function showLoseScreen() {
 
 document.querySelector(".level-ranking").addEventListener("click", () => {
     document
-        .querySelectorAll("[data-rank-type=level]")
+        .querySelectorAll("[data-rank-type=daily]")
         .forEach((entry) => (entry.style.display = "flex"));
     document
-        .querySelectorAll("[data-rank-type=global]")
+        .querySelectorAll("[data-rank-type=all-time]")
         .forEach((entry) => (entry.style.display = "none"));
 });
 
 document.querySelector(".global-ranking").addEventListener("click", () => {
     document
-        .querySelectorAll("[data-rank-type=global]")
+        .querySelectorAll("[data-rank-type=all-time]")
         .forEach((entry) => (entry.style.display = "flex"));
     document
-        .querySelectorAll("[data-rank-type=level]")
+        .querySelectorAll("[data-rank-type=daily]")
         .forEach((entry) => (entry.style.display = "none"));
 });
 
 document.querySelector(".retry-button").addEventListener("click", () => {
-    _triggerReason = 'Retry';
+    _triggerReason = "Retry";
     rewardEvent();
-    // level--;
-    // initBoard();
-    // closeModal("win-modal");
+    level--;
+    initBoard();
+    closeModal("win-modal");
 });
 
 document.querySelector(".next-button").addEventListener("click", () => {
-    _triggerReason = 'NextGame';
+    _triggerReason = "NextGame";
     rewardEvent();
-    // initBoard();
-    // closeModal("win-modal");
+    initBoard();
+    closeModal("win-modal");
 });
 
 // TODO: Add Whatsapp share stuff
 document
     .querySelector(".share-whatsapp-button")
-    .addEventListener("click", () => {});
+    .addEventListener("click", () => {
+        gtag("event", "gameShare", {
+            Type: "StatsShare",
+        });
+
+        let stars = 0;
+        levels.forEach(level => stars += level.starsCount);
+
+        const toCopy =
+            "#Dicele stats" +
+            "\n\n" +
+            "🎮 played - " +
+            level +
+            "\n" +
+            "⭐ stars - " +
+            stars +
+            "\n\n" +
+            "dicele.com 🎲";
+
+        if (navigator.clipboard && navigator.clipboard.writeText) {
+            navigator.clipboard.writeText(toCopy).then(
+                function () {
+                    console.debug("Copied to clipboard navigator: " + toCopy);
+                },
+                function (error) {
+                    console.error(
+                        "Failed to copy to clipboard navigator",
+                        error
+                    );
+                }
+            );
+        }
+
+        const isMobileOrTablet = () => {
+            let check = false;
+            (function (a) {
+                if (
+                    /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+                        a
+                    ) ||
+                    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+                        a.substr(0, 4)
+                    )
+                )
+                    check = true;
+            })(navigator.userAgent || navigator.vendor || window.opera);
+            return check;
+        };
+        if (isMobileOrTablet() && navigator.share) {
+            navigator
+                .share({
+                    text: toCopy,
+                })
+                .finally(() => document.getElementById("background").click());
+        }
+    });
 
 // Lose modal event listeners
 
 document.querySelector(".add-moves-button").addEventListener("click", () => {
-    _triggerReason = 'Reward';
+    _triggerReason = "Reward";
     rewardEvent();
-    // adCount++;
-    // updateLocalStorage();
-    // document.querySelector(".moves-number").innerHTML = moves + 5 * adCount;
-    // closeModal("lose-modal");
+    adCount++;
+    updateLocalStorage();
+    document.querySelector(".moves-number").innerHTML = moves + 5 * adCount;
+    closeModal("lose-modal");
 });
 
 document.querySelector(".try-again-button").addEventListener("click", () => {
-    _triggerReason = 'replay';
+    _triggerReason = "replay";
     rewardEvent();
 });
 
